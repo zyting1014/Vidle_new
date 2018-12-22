7979d65ca0d637fafe366f65c9a51a7f549a5fbf
QingyuLiu <16301097@bjtu.edu.cn>
2018-12-23 00:25:11 +0800

    提交

---
+++ app/src/main/java/com/demo/fish/app/main/PostGoods/PostGoodsActivity.java
@@ -0,0 +1,336 @@
+package com.demo.fish.app.main.PostGoods;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.demo.fish.R;
+import com.lidong.photopicker.PhotoPickerActivity;
+import com.lidong.photopicker.PhotoPreviewActivity;
+import com.lidong.photopicker.SelectModel;
+import com.lidong.photopicker.intent.PhotoPickerIntent;
+import com.lidong.photopicker.intent.PhotoPreviewIntent;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ * Created by xuejinwei on 16/8/19.
+ * Email:xuejinwei@outlook.com
+ */
+public class PostGoodsActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA_CODE = 10;
+    private static final int REQUEST_PREVIEW_CODE = 20;
+    private ArrayList<String> imagePaths = new ArrayList<>();
+
+    private GridView gridView;
+    private GridAdapter gridAdapter;
+    private String TAG =PostGoodsActivity.class.getSimpleName();
+
+    @BindView(R.id.tv_price)         TextView       tv_price;
+    @BindView(R.id.ll_price)         LinearLayout   ll_price;
+    @BindView(R.id.et_price)         EditText       et_price;
+    @BindView(R.id.et_orginal_price) EditText       et_orginal_price;
+    @BindView(R.id.et_freight)       EditText       et_freight;
+    @BindView(R.id.keyboard_view)    MyKeyBoardView keyboard_view;
+    @BindView(R.id.ll_price_select)  LinearLayout   ll_price_select;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_goods);
+        gridView = (GridView) findViewById(R.id.gridView);
+        ButterKnife.bind(this);
+
+        final KeyboardUtil keyboardUtil = new KeyboardUtil(PostGoodsActivity.this);
+        keyboardUtil.setOnOkClick(new KeyboardUtil.OnOkClick() {
+            @Override
+            public void onOkClick() {
+                if (validate()) {
+                    ll_price_select.setVisibility(View.GONE);
+                    tv_price.setText(et_price.getText() + "/价格，" + et_orginal_price.getText() + "/原价，" + et_freight.getText() + "/运费");
+                }
+            }
+        });
+
+        keyboardUtil.setOnCancelClick(new KeyboardUtil.onCancelClick() {
+            @Override
+            public void onCancellClick() {
+                ll_price_select.setVisibility(View.GONE);
+            }
+        });
+
+        ll_price.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                keyboardUtil.attachTo(et_price);
+                et_price.setFocusable(true);
+                et_price.setFocusableInTouchMode(true);
+                et_price.requestFocus();
+                ll_price_select.setVisibility(View.VISIBLE);
+                ll_price_select.bringToFront();
+            }
+        });
+
+        et_price.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                keyboardUtil.attachTo(et_price);
+                return false;
+            }
+        });
+        et_orginal_price.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                keyboardUtil.attachTo(et_orginal_price);
+                return false;
+            }
+        });
+        et_freight.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                keyboardUtil.attachTo(et_freight);
+                return false;
+            }
+        });
+
+
+        //申请危险权限
+        readExternalStorage(getWindow().getDecorView());
+        writeExternalStorage(getWindow().getDecorView());
+        showCamera(getWindow().getDecorView());
+
+        //上传图片或拍照部分
+        int cols = getResources().getDisplayMetrics().widthPixels / getResources().getDisplayMetrics().densityDpi;
+        cols = cols < 3 ? 3 : cols;
+        gridView.setNumColumns(cols);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                String imgs = (String) parent.getItemAtPosition(position);
+                if ("paizhao".equals(imgs) ){
+                    PhotoPickerIntent intent = new PhotoPickerIntent(PostGoodsActivity.this);
+                    intent.setSelectModel(SelectModel.MULTI);
+                    intent.setShowCarema(true); // 是否显示拍照
+                    intent.setMaxTotal(6); // 最多选择照片数量，默认为6
+                    intent.setSelectedPaths(imagePaths); // 已选中的照片地址， 用于回显选中状态
+                    startActivityForResult(intent, REQUEST_CAMERA_CODE);
+                }else{
+                    Toast.makeText(PostGoodsActivity.this,"1"+position,Toast.LENGTH_SHORT).show();
+                    PhotoPreviewIntent intent = new PhotoPreviewIntent(PostGoodsActivity.this);
+                    intent.setCurrentItem(position);
+                    intent.setPhotoPaths(imagePaths);
+                    startActivityForResult(intent, REQUEST_PREVIEW_CODE);
+                }
+            }
+        });
+        imagePaths.add("paizhao");
+        gridAdapter = new GridAdapter(imagePaths);
+        gridView.setAdapter(gridAdapter);
+        //tv_click.setOnClickListener(new View.OnClickListener() {
+        //   @Override
+        //   public void onClick(View v) {
+        //  }
+        //});
+    }
+
+    public boolean validate() {
+        if (et_price.getText().toString().equals("")) {
+            Toast.makeText(getApplication(), "价格不能为空", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (et_orginal_price.getText().toString().equals("")) {
+            Toast.makeText(getApplication(), "原价不能为空", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (et_freight.getText().toString().equals("")) {
+            Toast.makeText(getApplication(), "运费不能为空", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (ll_price_select.getVisibility() == View.VISIBLE) {
+            ll_price_select.setVisibility(View.GONE);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+    /**
+     * 动态请求权限
+     */
+    public void readExternalStorage(View view) {
+        PermissionUtils.requestPermission(this, PermissionUtils.CODE_READ_EXTERNAL_STORAGE, mPermissionGrant);
+    }
+
+    public void writeExternalStorage(View view) {
+        PermissionUtils.requestPermission(this, PermissionUtils.CODE_WRITE_EXTERNAL_STORAGE, mPermissionGrant);
+    }
+    public void showCamera(View view) {
+        Log.i(TAG, "Show camera button pressed. Checking permission.");
+        PermissionUtils.requestPermission(this, PermissionUtils.CODE_CAMERA, mPermissionGrant);
+    }
+    private PermissionUtils.PermissionGrant mPermissionGrant = new PermissionUtils.PermissionGrant() {
+        @Override
+        public void onPermissionGranted(int requestCode) {
+            switch (requestCode) {
+                case PermissionUtils.CODE_RECORD_AUDIO:
+                    Toast.makeText(PostGoodsActivity.this, "Result Permission Grant CODE_RECORD_AUDIO", Toast.LENGTH_SHORT).show();
+                    break;
+                case PermissionUtils.CODE_GET_ACCOUNTS:
+                    Toast.makeText(PostGoodsActivity.this, "Result Permission Grant CODE_GET_ACCOUNTS", Toast.LENGTH_SHORT).show();
+                    break;
+                case PermissionUtils.CODE_READ_PHONE_STATE:
+                    Toast.makeText(PostGoodsActivity.this, "Result Permission Grant CODE_READ_PHONE_STATE", Toast.LENGTH_SHORT).show();
+                    break;
+                case PermissionUtils.CODE_CALL_PHONE:
+                    Toast.makeText(PostGoodsActivity.this, "Result Permission Grant CODE_CALL_PHONE", Toast.LENGTH_SHORT).show();
+                    break;
+                case PermissionUtils.CODE_CAMERA:
+                    Toast.makeText(PostGoodsActivity.this, "Result Permission Grant CODE_CAMERA", Toast.LENGTH_SHORT).show();
+                    break;
+                case PermissionUtils.CODE_ACCESS_FINE_LOCATION:
+                    Toast.makeText(PostGoodsActivity.this, "Result Permission Grant CODE_ACCESS_FINE_LOCATION", Toast.LENGTH_SHORT).show();
+                    break;
+                case PermissionUtils.CODE_ACCESS_COARSE_LOCATION:
+                    Toast.makeText(PostGoodsActivity.this, "Result Permission Grant CODE_ACCESS_COARSE_LOCATION", Toast.LENGTH_SHORT).show();
+                    break;
+                case PermissionUtils.CODE_READ_EXTERNAL_STORAGE:
+                    Toast.makeText(PostGoodsActivity.this, "Result Permission Grant CODE_READ_EXTERNAL_STORAGE", Toast.LENGTH_SHORT).show();
+                    break;
+                case PermissionUtils.CODE_WRITE_EXTERNAL_STORAGE:
+                    Toast.makeText(PostGoodsActivity.this, "Result Permission Grant CODE_WRITE_EXTERNAL_STORAGE", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        PermissionUtils.requestPermissionsResult(this, requestCode, permissions, grantResults, mPermissionGrant);
+    }
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK) {
+            switch (requestCode) {
+                // 选择照片
+                case REQUEST_CAMERA_CODE:
+                    ArrayList<String> list = data.getStringArrayListExtra(PhotoPickerActivity.EXTRA_RESULT);
+                    Log.d(TAG, "数量："+list.size());
+                    loadAdpater(list);
+                    break;
+                // 预览
+                case REQUEST_PREVIEW_CODE:
+                    ArrayList<String> ListExtra = data.getStringArrayListExtra(PhotoPreviewActivity.EXTRA_RESULT);
+                    loadAdpater(ListExtra);
+                    break;
+            }
+        }
+    }
+
+    private void loadAdpater(ArrayList<String> paths){
+        if (imagePaths!=null&& imagePaths.size()>0){
+            imagePaths.clear();
+        }
+        if (paths.contains("paizhao")){
+            paths.remove("paizhao");
+        }
+        paths.add("paizhao");
+        imagePaths.addAll(paths);
+        gridAdapter  = new GridAdapter(imagePaths);
+        gridView.setAdapter(gridAdapter);
+        try{
+            JSONArray obj = new JSONArray(imagePaths);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private class GridAdapter extends BaseAdapter {
+        private ArrayList<String> listUrls;
+        private LayoutInflater inflater;
+        public GridAdapter(ArrayList<String> listUrls) {
+            this.listUrls = listUrls;
+            if(listUrls.size() == 7){
+                listUrls.remove(listUrls.size()-1);
+            }
+            inflater = LayoutInflater.from(PostGoodsActivity.this);
+        }
+
+        public int getCount(){
+            return  listUrls.size();
+        }
+        @Override
+        public String getItem(int position) {
+            return listUrls.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                convertView = inflater.inflate(R.layout.item, parent,false);
+                holder.image = (ImageView) convertView.findViewById(R.id.imageView);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder)convertView.getTag();
+            }
+
+            final String path=listUrls.get(position);
+            if (path.equals("paizhao")){
+                holder.image.setImageResource(R.mipmap.find_add_img);
+            }else {
+                Glide.with(PostGoodsActivity.this)
+                        .load(path)
+                        .into(holder.image);
+            }
+            return convertView;
+        }
+        class ViewHolder {
+            ImageView image;
+        }
+    }
+}
